8406bfd246824a4f6d28efc12d11386275f26ee2
spindola001 <marcos.spindola001@gmail.com>
2024-09-03 20:00:04 -0300

    First Commit

---
+++ Array/ArrayExercicios.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EstruturaDeDados.Array
+{
+    public class ArrayExercicios
+    {
+        public string[] arrayStringExample { get; set; }
+        public int[] arrayIntegerExample { get; set; }
+
+        public ArrayExercicios()
+        {
+            arrayIntegerExample = [0, 20, 19, 4, 6, 14, 10];
+            arrayStringExample = ["Evelyn", "Marcos", "Cecília", "Zacarias", "Salsicha", "João", "Zacarias", "Thomas"];
+        }
+
+        /// <summary>
+        /// Linear Search - String Data
+        /// </summary>
+        /// <param name="arrayStringExample"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public int LinearSearchAlgStringData(string[] arrayStringExample, string filter)
+        {
+            for (int i = 0; i < arrayStringExample.Length; i++)
+            {
+                if (arrayStringExample[i].Equals(filter, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Insertion Sort - String Data
+        /// Complexidade de Tempo: O(n²) no caso médio e pior caso, O(n) no melhor caso.
+        ///     - Em um caso onde o Array está em ordem decrescente, o loop for irá comparar cada elemento com seus elementos anteriores.
+        /// Complexidade de Espaço: O(1)
+        /// </summary>
+        /// <param name="arrayStringExample"></param>
+        /// <returns></returns>
+        public string[] InsertionSortAlgStringData(string[] arrayStringExample)
+        {
+            string aux;
+            int j;
+
+            for (int i = 1; i < arrayStringExample.Length; i++)
+            {
+                aux = arrayStringExample[i];
+                j = i - 1;
+
+                int currentFirstLetter = char.ToLower(aux[0]);
+
+                while (j >= 0 && char.ToLower(arrayStringExample[j][0]) > currentFirstLetter)
+                {
+                    arrayStringExample[j + 1] = arrayStringExample[j];
+                    j--;
+                }
+
+                arrayStringExample[j + 1] = aux;
+            }
+
+            return arrayStringExample;
+        }
+
+        /// <summary>
+        /// Linear Search - Int Data
+        /// </summary>
+        /// <param name="arrayIntegerExample"></param>
+        /// <param name="filter"></param>
+        /// <returns></returns>
+        public int LinearSearchAlgIntegerData(int[] arrayIntegerExample, int filter)
+        {
+            for (int i = 0; i < arrayIntegerExample.Length; i++)
+            {
+                if (arrayIntegerExample[i] == filter)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Insertion Sort - Int Data
+        /// Complexidade de Tempo: O(n²) no caso médio e pior caso, O(n) no melhor caso.
+        ///     - Em um caso onde o Array está em ordem decrescente, o loop for irá comparar cada elemento com seus elementos anteriores.
+        /// Complexidade de Espaço: O(1)
+        /// </summary>
+        /// <param name="arrayIntegerExample"></param>
+        /// <returns></returns>
+        public int[] InsertionSortAlgIntegerData(int[] arrayIntegerExample)
+        {
+            int aux, j;
+
+            for (int i = 1; i < arrayIntegerExample.Length; i++)
+            {
+                aux = arrayIntegerExample[i];
+                j = i - 1;
+
+                while (j >= 0 && arrayIntegerExample[j] > aux)
+                {
+                    arrayIntegerExample[j + 1] = arrayIntegerExample[j];
+                    j--;
+                }
+
+                arrayIntegerExample[j + 1] = aux;
+            }
+
+            return arrayIntegerExample;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using EstruturaDeDados.Array;
+
+namespace EstruturaDeDados
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------------------------- INICIO DE EXECUÇÃO DE EXERCÍCIOS DE ESTRUTURA DE DADOS ---------------------------------------------------------------");
+            Console.WriteLine("ESCOLHA UMA OPÇÃO DE EXERCÍCIO PARA EXECUTAR: ");
+            Console.WriteLine("1- BUSCA EM UM ARRAY DE INTEIROS");
+            Console.WriteLine("2- ORDENAR UM ARRAY DE INTEIROS");
+            Console.WriteLine("3- BUSCA EM UM ARRAY DE STRINGS");
+            Console.WriteLine("4- ORDENAR UM ARRAY DE STRINGS");
+            Console.WriteLine("0- Sair");
+            Console.WriteLine("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+            int.TryParse(Console.ReadLine(), out int option);
+            
+            var arrMethods = new ArrayExercicios();
+
+            ExecuteMethod(option, arrMethods);
+        }
+
+        public static void ExecuteMethod(int option, ArrayExercicios parameters)
+        {
+            switch (option)
+            {
+                case 1:
+                    {
+                        var result = parameters.LinearSearchAlgIntegerData(parameters.arrayIntegerExample, 14);
+                        if (result < 0)
+                            Console.WriteLine("Não encontramos o valor dentro do Array...");
+                        else
+                            Console.WriteLine($"Valor encontrado na posição {result} do array!");
+                    }
+                    break;
+                case 2:
+                    {
+                        var result = string.Join(", ", parameters.InsertionSortAlgIntegerData(parameters.arrayIntegerExample));
+                        Console.WriteLine($"Array reordenado: [ {result} ]");
+                    }
+                    break;
+                case 3:
+                    {
+                        var result = parameters.LinearSearchAlgStringData(parameters.arrayStringExample, "Marcio");
+                        if (result < 0)
+                            Console.WriteLine("Não encontramos o valor dentro do Array...");
+                        else
+                            Console.WriteLine($"Valor encontrado na posição {result} do array!");
+                    }
+                    break;
+                case 4:
+                    {
+                        var result = string.Join(", ", parameters.InsertionSortAlgStringData(parameters.arrayStringExample));
+                        Console.WriteLine($"Array reordenado: [ {result} ]");
+                    }
+                    break;                    
+                default:
+                    break;
+            }
+        }
+    }
+}
